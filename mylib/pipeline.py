af740cdff92bbf597f5280a6bc35333d90bec1de
Guillem Mirabent Rubinat <guillem.mirabent@bse.eu>
2023-12-09 20:06:53 +0100

    Organizing the code so that the pipeline can be run from the notebook.

--- mylib/pipeline.py
+++ mylib/pipeline.py
@@ -1,19 +1,24 @@
 
-from preprocessing import load_data, handle_missing_values
-from features import feature_engineering_step1, feature_engineering_step2
-from model import train_model, evaluate_model
+from .preprocessing import load_data, handle_missing_values, drop_string_columns
+from .features import clean_player_traits, clean_body_type, process_work_rate, calculate_years_until_expiry, calculate_age, calculate_years_in_club
+from .model import train_model, evaluate_model, train_test_split_data
 
 def run_pipeline(data_path):
     '''Run the end-to-end pipeline.'''
     # Load data
     data = load_data(data_path)
 
-    # Preprocess data
-    data = handle_missing_values(data)
-
     # Feature engineering
-    data = feature_engineering_step1(data)
-    data = feature_engineering_step2(data)
+    data = clean_player_traits(data)
+    data = clean_body_type(data)
+    data = process_work_rate(data)
+    data = calculate_years_until_expiry(data)
+    data = calculate_age(data)
+    data = calculate_years_in_club(data)
+    
+        # Preprocess data
+    data = drop_string_columns(data)
+    data = handle_missing_values(data)
 
     # Split data into train and test
     X_train, X_test, y_train, y_test = train_test_split_data(data)
--- mylib/preprocessing.py
+++ mylib/preprocessing.py
@@ -8,6 +8,10 @@ def load_data(file_path):
     '''Load data from a CSV file.'''
     return pd.read_csv(file_path)
 
+def drop_string_columns(data):
+    '''Drop string columns from the dataset.'''
+    return data.select_dtypes(exclude=['object'])
+
 def handle_missing_values(data):
     '''Handle missing values in the dataset.'''
     # Example: Fill missing values with the median or mean
