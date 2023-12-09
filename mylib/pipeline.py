a8a8b9b4cf852f2dc136095c9e914c43d054912c
Guillem Mirabent Rubinat <guillem.mirabent@bse.eu>
2023-12-09 21:21:34 +0100

    added label encoder to target variable

--- mylib/pipeline.py
+++ mylib/pipeline.py
@@ -1,5 +1,5 @@
 
-from .preprocessing import load_data, handle_missing_values, drop_string_columns
+from .preprocessing import load_data, handle_missing_values, drop_string_columns, numerical_encoder
 from .features import clean_player_traits, clean_body_type, process_work_rate, calculate_years_until_expiry, calculate_age, calculate_years_in_club
 from .model import train_model, evaluate_model, train_test_split_data
 
@@ -7,6 +7,10 @@ def run_pipeline(data_path):
     '''Run the end-to-end pipeline.'''
     # Load data
     data = load_data(data_path)
+    
+    target = 'position'
+    
+    data[target], encoder_dict, decoder_dict = numerical_encoder(data[target])
 
     # Feature engineering
     data = clean_player_traits(data)
@@ -17,8 +21,8 @@ def run_pipeline(data_path):
     data = calculate_years_in_club(data)
     
         # Preprocess data
-    #data = drop_string_columns(data)
     data = handle_missing_values(data)
+    data = drop_string_columns(data)
 
     # Split data into train and test
     X_train, X_test, y_train, y_test = train_test_split_data(data)
--- mylib/preprocessing.py
+++ mylib/preprocessing.py
@@ -20,11 +20,21 @@ def handle_missing_values(data):
     # Separate the data into string and non-string columns
     string_data = data.select_dtypes(include=['object'])
     non_string_data = data.select_dtypes(exclude=['object'])
-
-    # Handle missing values in non-string columns only
+    
     non_string_data_filled = non_string_data.fillna(non_string_data.median())
-
-    # Concatenate the string and filled non-string data
+    
     data_filled = pd.concat([non_string_data_filled, string_data], axis=1)
-
+    
     return data_filled
+
+def numerical_encoder(data):
+    '''Numerically encode the specified column in the dataset.
+    Return both the encoded column, the encoder and the decoder.'''
+    encoder = LabelEncoder()
+    encoded_column = encoder.fit_transform(data)
+    
+    # Create a mapping dictionary from encoded labels to original labels
+    encoder_mapping = dict(zip(encoder.classes_, range(len(encoder.classes_))))
+    decoder_mapping = dict(zip(range(len(encoder.classes_)), encoder.classes_))
+    
+    return encoded_column, encoder_mapping, decoder_mapping
